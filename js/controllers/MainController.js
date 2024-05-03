6257fa49b48a355ad76e677de30921af806fc57c
LorriU <l.ugen@hotmail.co.uk>
2024-05-03 16:34:55 -0400

    added main folders and files

---
+++ js/controllers/MainController.js
@@ -0,0 +1,6 @@
+app.controller('MainController', ['$scope', function($scope) { 
+    $scope.title = 'Top Sellers in Books to Buy';
+    $scope.promo = 'You dont want to miss these deals'; 
+  
+  }]);
+  
